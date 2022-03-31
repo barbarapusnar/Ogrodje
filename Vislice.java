8eef6b8ab9dc90e2587ba5742eb2e5d513425c2b
Barbara Pušnar <barbara.pusnar@scng.si>
2022-03-31 08:00:51 +0200

    vvvv

---
+++ Vislice.java
@@ -0,0 +1,23 @@
+
+
+public class Vislice {
+    private String skritaBeseda;
+    public Vislice() {
+        String[] vsebesede=
+        {
+            "MATEMATIKA",
+            "ASTRONOMIJA",
+            "ALIGATOR",
+            "OTORINOLARINGOLOGIJA",
+            "KORONA",
+            "LOCKDOWN",
+            "UKRAJINA",
+            "RAKETOMET",
+            "KARANTENA",
+            "IZOLACIJA"
+        };
+        int n=(int)(Math.random()*10);
+        skritaBeseda=vsebesede[n];
+        ///
+    }
+}
